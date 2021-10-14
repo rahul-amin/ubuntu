0e42e231a28b00d567c0715e383460d3f45a20ad
rahul amin roktim <rahulaminroktim@gmail.com>
2021-10-14 12:03:36 +0600

    Create install php.sh

---
+++ install php.sh
@@ -0,0 +1,7 @@
+sudo apt install php libapache2-mod-php
+sudo systemctl restart apache2
+sudo apt install php-fpm
+# systemctl status php7.2-fpm
+systemctl status php7.4-fpm
+
+sudo apt install php-mysql php-gd
