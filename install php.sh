ff07837a17a81a444752c193be9189d4804492e4
rahul amin roktim <rahulaminroktim@gmail.com>
2021-10-14 15:26:15 +0600

    Update install php.sh

--- install php.sh
+++ install php.sh
@@ -3,5 +3,10 @@ sudo systemctl restart apache2
 sudo apt install php-fpm
 # systemctl status php7.2-fpm
 systemctl status php7.4-fpm
-
 sudo apt install php-mysql php-gd
+
+
+sudo apt install mysql-server
+sudo mysql_secure_installation
+mysql -u root -p
+systemctl status mysql.service
