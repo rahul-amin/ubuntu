80bc76729a0c9b98497dbe7659b8bd26e27fc28b
rahul amin roktim <rahulaminroktim@gmail.com>
2021-10-14 15:33:01 +0600

    Update install php.sh

--- install php.sh
+++ install php.sh
@@ -10,3 +10,10 @@ sudo apt install mysql-server
 sudo mysql_secure_installation
 mysql -u root -p
 systemctl status mysql.service
+
+sudo ln -s /usr/share/phpmyadmin /var/www/
+
+sudo nano /etc/apache2/apache2.conf
+## add this line inside->
+# Include /etc/phpmyadmin/apache.conf
+sudo service apache2 reload
