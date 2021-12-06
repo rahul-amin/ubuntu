042821a828cf86401847cecd939a602967b27f4b
rahul amin roktim <rahulaminroktim@gmail.com>
2021-12-06 17:38:55 +0600

    Update run background.sh

--- run background.sh
+++ run background.sh
@@ -1,6 +1,7 @@
-aria2c --enable-rpc --rpc-listen-all --enable-http-keep-alive &
-jobs
+aria2c --enable-rpc --rpc-listen-all --enable-http-keep-alive --rpc-allow-origin-all &
 disown  -h  %1
 jobs
 
 nohup aria2c --enable-rpc --rpc-listen-all --enable-http-keep-alive &
+
+# add & after command
